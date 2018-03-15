ac29a2b6a2981eb8af30b0dc1adb3caea154468f
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2018-03-15 11:19:57 -0400

    added an exec to run the makedag.js

--- gitwatch.js
+++ gitwatch.js
@@ -1,9 +1,13 @@
 var filewatcher = require('filewatcher');
 
 var watcher = filewatcher();
+var child;
+
 watcher.add(__dirname + "/.git/objects");
 
 watcher.on('change', function(file, stat) {
+
+	child = exec("node makedag.js")
   console.log('File modified: %s', file);
   if (!stat) console.log('deleted');
-});
+});("node makedag.js")
